c1ae1d6f0eb0a11db6930dc9b9fb34296d02033d
junghongseop <a01082372487@gmail.com>
2025-06-22 22:09:52 +0900

    feat: 기호 객체 생성

---
+++ constants/constant.ts
@@ -0,0 +1,23 @@
+import { SvgProps } from "react-native-svg";
+import Icon1 from "../components/icon/Icon1";
+import Icon2 from "../components/icon/Icon2";
+import Icon3 from "../components/icon/Icon3";
+import Icon4 from "../components/icon/Icon4";
+import Icon5 from "../components/icon/Icon5";
+import Icon6 from "../components/icon/Icon6";
+import Icon7 from "../components/icon/Icon7";
+import Icon8 from "../components/icon/Icon8";
+import Icon9 from "../components/icon/Icon9";
+
+export const SYMBOL_MAP: Record<number, React.FC<SvgProps>> = {
+  1: Icon1,
+  2: Icon2,
+  3: Icon3,
+  4: Icon4,
+  5: Icon5,
+  6: Icon6,
+  7: Icon7,
+  8: Icon8,
+  9: Icon9,
+};
+
